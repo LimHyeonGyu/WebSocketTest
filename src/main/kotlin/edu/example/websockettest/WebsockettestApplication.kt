8f20adffbf735e803866a1d29086cd9b34299de2
LimHyeunGyu <delldro94@gmail.com>
2024-11-14 18:29:26 +0900

    first commit

---
+++ src/main/kotlin/edu/example/websockettest/WebsockettestApplication.kt
@@ -0,0 +1,11 @@
+package edu.example.websockettest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebsockettestApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebsockettestApplication>(*args)
+}
---
+++ src/test/kotlin/edu/example/websockettest/WebsockettestApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.example.websockettest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebsockettestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
